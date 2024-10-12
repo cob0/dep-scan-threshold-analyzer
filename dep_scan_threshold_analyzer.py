39cadfb2ed5172c6e9798b8228b0135b6d2a2673
cob0 <cob0@tutanota.com>
2024-10-12 21:18:24 +0200

    feature #8: add argument parser for filename and threshold args

--- dep_scan_threshold_analyzer.py
+++ dep_scan_threshold_analyzer.py
@@ -1,12 +1,13 @@
 import sys
 
+from log.logger import print_logo
+
+print_logo()
+
+from argparser.config.argsconfig import args
 from depscan.analyzer.thresholdanalyzer import ThresholdAnalyzer
 
-# get file report location from environment variable
-report_file_src = sys.argv[1]
-cvss_score_threshold = float(sys.argv[2])
-
-analyzer = ThresholdAnalyzer(report_file_src, cvss_score_threshold)
+analyzer = ThresholdAnalyzer(args.file, args.threshold)
 
 if analyzer.run() is True:
     sys.exit(1)
